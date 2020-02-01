baa891a847fb984a44e344a8bc1e488c479484c2
RIchard Thomas <rthomas4393@student.landstede.nl>
2020-02-01 06:31:20 +0100

    Updates

---
+++ precache-manifest.38606aec4c7b2540385985baa326e145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bd606c953ba1f6cdee8ad20a8ffc4b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f8c4bafb2225cadfcb62",
+    "url": "/monsters-rolodex/static/css/main.74bf8990.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8c4bafb2225cadfcb62",
+    "url": "/monsters-rolodex/static/js/main.e4b9ec7c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
